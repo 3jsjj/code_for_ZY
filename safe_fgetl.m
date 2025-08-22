984aa8136e0d029c0039de5489f2c4df725db4de
zhangxin <12432359@mail.sustech.ecu.cn>
2025-08-22 22:56:42 +0800

    git commit -m "Update read.txt with new content"

--- safe_fgetl.m
+++ safe_fgetl.m
@@ -1,13 +1,12 @@
-
-function safe_line = safe_fgetl(fid)
-	temp_line = fgetl(fid);
-	if ~ischar(temp_line)
-		if temp_line == -1
-			temp_line='';
-		else
-			error('not end');
-		end
-	end
-	safe_line = strtrim(temp_line);
+function safe_line = safe_fgetl(fid) 
+    temp_line = fgetl(fid); 
+    if ~ischar(temp_line) 
+        if temp_line == -1 
+            safe_line = -1;  % 保持原来的 -1，不要转换成空字符串
+            return;
+        else 
+            error('not end'); 
+        end 
+    end 
+    safe_line = strtrim(temp_line); 
 end
-
