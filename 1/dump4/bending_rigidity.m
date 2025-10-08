27e74969f3497607231ab4fd84ecd769b6339541
zhangxin <12432359@mail.sustech.ecu.cn>
2025-10-08 21:27:37 +0800

    git commit -m "move files into folder 1p8"

--- 1/dump4/bending_rigidity.m
+++ 1/dump4/bending_rigidity.m
@@ -10,8 +10,8 @@
 
 flag1 =1;
 pb=1;pe=15;
-value = 3.01;
-value1 = 0.95;
+value = 2.51;
+value1 = 0.98;
 Lx = 70 * value1;
 Ly = 40 * value1 *1.73 ;
 n = round(Lx)* 2;
@@ -22,7 +22,7 @@ natoms=5609;
 
 for i=2:2
 
-prefix = sprintf('dump_0.95_%s_228_onlymembrane', num2str(value));
+prefix = sprintf('dump_0.98_%s_228_onlymembrane', num2str(value));
 filename = [prefix '.lammpstrj'];
 
 
